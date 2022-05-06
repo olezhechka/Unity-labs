edc8fc640bb9bec884f2fe73bc4039ac1cb925ed
Oleg Kyssa <x7.sunjin@gmail.com>
2022-05-06 12:02:22 +0300

    Laba-6

--- Assets/GrindBalance/GrindBalanceTable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptable Objects/Grid Balance Table", fileName = "New Grind Balance Table")]
-public class GrindBalanceTable : ScriptableObject
-{
-    public GrindBalanceModel[] grindBalanceData;
-}
